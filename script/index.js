1c18e0b8057ace636961de2e9f0ef11fe7f2be1e
eudondian <eudondian@gmail.com>
2022-06-24 08:32:54 +0100

    Fix linter error

--- script/index.js
+++ script/index.js
@@ -1,16 +1,40 @@
-const hamburger = document.querySelector('.mobile-menu');
-const closeHamburger = document.querySelector('.desktop-nav-hidden');
-const nav = document.querySelector('.desktop-nav-hidden');
-const closeNav = document.querySelector('.desktop-nav-hidden');
+// Toggle Nav Menu
 
-hamburger.addEventListener('click', () => {
-  nav.classList.add('openMenu');
+const menu = document.querySelector('.nav__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+const cardBtn = document.querySelectorAll('.card-button');
+const navLink = document.querySelector('#nav-menu');
+const popBtn = document.querySelector('.close-btn');
+const popUp = document.querySelector('#popup-1');
+
+menuBtn.addEventListener('click', () => {
+  menu.style.display = 'flex';
+  closeBtn.style.display = 'inline-block';
+  menuBtn.style.display = 'none';
+  menu.style.zIndex = '10';
+  closeBtn.style.zIndex = '11';
 });
 
-closeHamburger.addEventListener('click', () => {
-  nav.classList.remove('openMenu');
+// Close the nav menu
+
+const closeNav = () => {
+  menu.style.display = 'none';
+  closeBtn.style.display = 'none';
+  menuBtn.style.display = 'inline-block';
+};
+// close nav menu on clicking close button
+closeBtn.addEventListener('click', closeNav);
+
+// Close nav menu on clicking any link item
+navLink.addEventListener('click', closeNav);
+
+cardBtn.forEach((item) => {
+  item.addEventListener('click', () => {
+    document.querySelector('#popup-1').classList.toggle('active');
+  });
 });
 
-closeNav.addEventListener('click', () => {
-  nav.classList.remove('openMenu');
+popBtn.addEventListener('click', () => {
+  popUp.style.display = 'none';
 });
